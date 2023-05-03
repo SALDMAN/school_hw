1cd81601e9168afe184fd5e86f004598fd927360
SALDMAN <109432390+SALDMAN@users.noreply.github.com>
2023-05-03 23:09:41 +0300

    Add files via upload

--- BankAccount.java
+++ BankAccount.java
@@ -1,10 +1,61 @@
-package classs;
-
 public class BankAccount {
+ private String accountName;
+ private String accountNum;
+ private double balance;
+ private boolean isOverDraftAllowed;
+ private double overDraftSum;
+ public BankAccount(){
+     accountName="account";
+     accountNum="000001";
+     balance=0;
+     isOverDraftAllowed=false;
+     overDraftSum=0;
+ }
+ public BankAccount(String account,String num,double balance,boolean flag,double sum){
+     this.accountName=account;
+     this.accountNum=num;
+     this.balance=balance;
+     this.isOverDraftAllowed=flag;
+     this.overDraftSum=sum;
+ }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public String getAccountName() {
+        return accountName;
+    }
 
-	}
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
 
+    public String getAccountNum() {
+        return accountNum;
+    }
+
+    public void setAccountNum(String accountNum) {
+        this.accountNum = accountNum;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public boolean isOverDraftAllowed() {
+        return isOverDraftAllowed;
+    }
+
+    public void setOverDraftAllowed(boolean overDraftAllowed) {
+        isOverDraftAllowed = overDraftAllowed;
+    }
+
+    public double getOverDraftSum() {
+        return overDraftSum;
+    }
+
+    public void setOverDraftSum(double overDraftSum) {
+        this.overDraftSum = overDraftSum;
+    }
 }
