b20a1b2a18e61d96a802cb2f3b58012f9974791f
SALDMAN <109432390+SALDMAN@users.noreply.github.com>
2023-05-11 16:27:21 +0300

    Update rashi.java

--- rashi.java
+++ rashi.java
@@ -3,7 +3,13 @@ public class rashi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+    Color c1=new Color("black",5.3,"pen");
+    Color c2=new Color("blue",2.4,"ret");
+    Color c3=new Color("yellow",6.7,"tosh");
+    Colorbox arr =new Colorbox(3);
+    arr.addcolor(c1);
+    arr.addcolor(c2);
+    arr.addcolor(c3);
 	}
 
 }
